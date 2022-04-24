25959d474fa773fc1ca5e5a3344145101eceb38a
AppoGit-Hub <bl@dtd.be>
2022-04-24 12:11:13 +0200

    added awake, args to update, onEvent, onKeyPressed, add & get Known, hasComponent, ensureComponent, awake, invokeEvent, invokeKeyPressed

--- ecs.py
+++ ecs.py
@@ -3,13 +3,23 @@ class Component:
     def __init__(self):
         self.id = 0
 
+    def awake(self):
+        pass
+
     def start(self):
         pass
 
-    def update(self):
+    def update(self, deltaTime, *args):
+        pass
+
+    def onEvent(self, deltaTime, event):
+        pass
+
+    def onKeyPressed(self, deltaTime, onKeyPressed):
         pass
 
 GameObject = dict[type : Component]
+KNOWN_ID = 0
 
 def components_to_gameObject(id, components : list[Component]) -> GameObject:
     gameObject : GameObject = {}
@@ -25,25 +35,48 @@ class Scene:
     def __init__(self):
         self.nextGameobjectID = 0
         self.sceneID = Scene._next_scene_id
-        Scene._all_scenes.update({self.sceneID : {}})
+        Scene._all_scenes.update({self.sceneID : {} })
+        self.addGameObject([])
         Scene._next_scene_id += 1
 
+    def addKnownGlobal(sceneID : int, component : Component):
+        Scene.addComponentGlobal(sceneID, KNOWN_ID, component)
+
+    def addKnownCurrentScene(component : Component):
+        Scene.addKnownGlobal(Scene._current_loaded_scene_id, component)
+
+    def addKnown(self, component : Component):
+        Scene.addKnownGlobal(self.sceneID, component)
+
+    def getKnownGlobal(sceneID : int, component : Component):
+        return Scene.getComponentGlobal(sceneID, KNOWN_ID, component)
+
+    def getKnownCurrentScene(component : Component):
+        return Scene.getKnownGlobal(Scene._current_loaded_scene_id, component)
+
+    def getKnown(self, component : Component):
+        return Scene.getKnownGlobal(self.sceneID, component)
+
     def addGameObjectGlobal(sceneID : int, gameObject : list[Component]) -> int:
-        nextGameobjectID = Scene._all_scenes[sceneID].nextGameobjectID
+        nextGameobjectID = 0
+        if (len(Scene._all_scenes[sceneID]) > 0):
+            nextGameobjectID = list(Scene._all_scenes[sceneID].keys())[-1] + 1
         Scene._all_scenes[sceneID].update({
             nextGameobjectID : components_to_gameObject(nextGameobjectID, gameObject)
         })
-        nextGameobjectID += 1
-        return nextGameobjectID - 1
+        for component in gameObject: component.awake()
+        for component in gameObject: component.start()
+        return nextGameobjectID
 
     def addGameObjectCurrentScene(gameObject : list[Component]):
-        return Scene.addGameObjectGlobal(Scene._next_scene_id, gameObject)
+        return Scene.addGameObjectGlobal(Scene._current_loaded_scene_id, gameObject)
 
     def addGameObject(self, gameObject : list[Component]):
         return  Scene.addGameObjectGlobal(self.sceneID, gameObject)
 
     def removeGameObjectGlobal(sceneID : int, gameObjectID : int):
-        del Scene._all_scenes[sceneID][gameObjectID]
+        scene : dict = Scene._all_scenes[sceneID]
+        scene.pop(gameObjectID, None)
 
     def removeGameObjectCurrentScene(gameObjectID : int):
         Scene.removeGameObjectGlobal(Scene._current_loaded_scene_id, gameObjectID)
@@ -51,15 +84,35 @@ class Scene:
     def removeGameObject(self, gameObjectID : int):
         Scene.removeGameObjectGlobal(self.sceneID, gameObjectID)
 
-    def addComponentGlobal(sceneID : int, gameObjectID : int, componentInstance : int):
+    def addComponentGlobal(sceneID : int, gameObjectID : int, componentInstance : Component):
         gameObject = Scene._all_scenes[sceneID][gameObjectID]
         gameObject.update({type(componentInstance) : componentInstance})      
 
-    def addComponentCurrentScene(gameObjectID : int, componentInstance : int):
+    def addComponentCurrentScene(gameObjectID : int, componentInstance : Component):
         Scene.addComponentGlobal(Scene._current_loaded_scene_id, gameObjectID, componentInstance)
 
-    def addComponent(self, gameObjectID : int, componentInstance : int):
+    def addComponent(self, gameObjectID : int, componentInstance : Component):
         Scene.addComponentGlobal(self.sceneID, gameObjectID, componentInstance)
+
+    def ensureComponentGlobal(sceneID : int, gameObjectID : int, componentInstance : Component):
+        if not Scene.hasComponentGlobal(sceneID, gameObjectID, componentInstance):
+            Scene.addComponentGlobal(sceneID, gameObjectID, componentInstance)
+        return Scene.getComponentGlobal(sceneID, gameObjectID, componentInstance)
+
+    def ensureComponentCurrentScene(gameObjectID : int, componentInstance : Component):
+        Scene.ensureComponentGlobal(Scene._current_loaded_scene_id, gameObjectID, componentInstance)
+
+    def ensureComponent(self, gameObjectID : int, componentInstance : Component):
+        Scene.ensureComponentGlobal(self.sceneID, gameObjectID, componentInstance)
+
+    def hasComponentGlobal(sceneID : int, gameObjectID : int, componentInstance : Component):
+        return componentInstance in Scene._all_scenes[sceneID][gameObjectID]
+
+    def hasComponentCurrentScene(gameObjectID : int, componentInstance : Component):
+        return componentInstance in Scene._all_scenes[Scene._current_loaded_scene_id][gameObjectID]
+
+    def hasComponent(self, gameObjectID : int, componentInstance : Component):
+        return componentInstance in Scene._all_scenes[self.sceneID][gameObjectID]
 
     def getComponentGlobal(sceneID : int, gameObjectID : int, type : type) -> Component:
         return Scene._all_scenes[sceneID][gameObjectID][type]
@@ -72,9 +125,22 @@ class Scene:
 
     def setLoadScene(scene : 'Scene'):
         Scene._current_loaded_scene_id = scene.sceneID
+        scene.awake()
+        scene.start()
+
+    def awakeGlobal(sceneID : int):
+        for gameObject in list(Scene._all_scenes[sceneID].values()): 
+            for component in gameObject.values():
+                component.awake()
+
+    def awakeCurrentScene():
+        Scene.awakeGlobal(Scene._current_loaded_scene_id)
+
+    def awake(self):
+        Scene.awakeGlobal(self.sceneID)
 
     def startGlobal(sceneID : int):
-        for gameObject in Scene._all_scenes[sceneID].values():
+        for gameObject in list(Scene._all_scenes[sceneID].values()):
             for component in gameObject.values():
                 component.start()        
 
@@ -84,13 +150,35 @@ class Scene:
     def start(self):
         Scene.startGlobal(self.sceneID)
     
-    def updateGlobal(sceneID : int):
-        for gameObject in  Scene._all_scenes[sceneID].values():
+    def updateGlobal(sceneID : int, deltaTime : float, *args):
+        for gameObject in list(Scene._all_scenes[sceneID].values()):
             for component in gameObject.values():
-                component.update()       
+                component.update(deltaTime, *args)       
 
-    def updateCurrentScene():
-        Scene.updateGlobal(Scene._current_loaded_scene_id)
+    def updateCurrentScene(deltaTime : float, *args):
+        Scene.updateGlobal(Scene._current_loaded_scene_id, deltaTime, *args)
 
-    def update(self):
-        Scene.updateGlobal(self.sceneID)
+    def update(self, deltaTime : float, *args):
+        Scene.updateGlobal(self.sceneID, deltaTime, *args)
+
+    def invokeEventGlobal(sceneID : int, deltaTime, event):
+        for gameObject in list(Scene._all_scenes[sceneID].values()):
+            for component in gameObject.values():
+                component.onEvent(deltaTime, event) 
+
+    def invokeEventCurrentScene(deltaTime : float, event):
+        Scene.invokeEventGlobal(Scene._current_loaded_scene_id, deltaTime, event)
+
+    def invokeEvent(self, deltaTime : float, event):
+        Scene.invokeEventGlobal(self.sceneID, deltaTime, event)
+    
+    def invokeKeyPressedGlobal(sceneID : int, deltaTime : float, keyPressed : list):
+        for gameObject in list(Scene._all_scenes[sceneID].values()):
+            for component in gameObject.values():
+                component.onKeyPressed(deltaTime, keyPressed)
+
+    def invokeKeyPressedCurrentScene(deltaTime : float, keyPressed : list):
+        Scene.invokeKeyPressedGlobal(Scene._current_loaded_scene_id, deltaTime, keyPressed)
+    
+    def invokeKeyPressed(self, deltaTime : float, keyPressed : list):
+        Scene.invokeKeyPressedGlobal(self.sceneID, deltaTime, keyPressed)
